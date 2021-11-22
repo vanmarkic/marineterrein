6a0da2fba0b3e22d702fccb81bba80179e972423
Dragan Markovic <drag.markovic@gmail.com>
2021-11-22 10:49:02 +0100

    added labels for data layers

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,35 @@
-import * as React from "react"
-import styled from "styled-components"
-import breakpoint from '../styles/globalStyle'
+import * as React from "react";
+import styled from "styled-components";
+import breakpoint from "../styles/globalStyle";
 
-import ZonesColors from '../components/zonesColors'
+import ZonesColors from "../components/zonesColors";
 // import ZonesRandomPoints from '../components/zonesRandomPoints'
-import ZonesRandomPoints from '../components/zonesRandomPointsV2'
+import ZonesRandomPoints from "../components/zonesRandomPointsV2";
 
-import "../styles/index.css"
+import "../styles/index.css";
 
-const MainWrapper =styled.div`
-    margin:auto;
-    @media only screen and ${breakpoint.device.xs}{ 
-       margin: auto;
-      width: 95vw;
-    }
-    @media only screen and ${breakpoint.device.sm}{
+const MainWrapper = styled.div`
+  margin: auto;
+  @media only screen and ${breakpoint.device.xs} {
+    margin: auto;
+    width: 95vw;
+  }
+  @media only screen and ${breakpoint.device.sm} {
+  }
+  @media only screen and ${breakpoint.device.lg} {
+    padding: 30px 30px;
+    max-height: 98vh;
+  }
+`;
 
-    }
-    @media only screen and ${breakpoint.device.lg}{
-        padding: 30px 30px; 
-        max-height: 99vh;
-    }
-`
-
-// markup 
+// markup
 const IndexPage = () => {
-
   return (
-    <MainWrapper >
+    <MainWrapper>
       <ZonesColors />
       {/* <ZonesRandomPoints /> */}
     </MainWrapper>
   );
-}
+};
 
-export default IndexPage
+export default IndexPage;
